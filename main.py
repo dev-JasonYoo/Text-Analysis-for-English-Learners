6902cce82b94074c4861df2c010d85bcb3136b72
dev-JasonYoo <jasonshyoo@gmail.com>
2021-10-01 13:16:19 +0000

    Applied openpyxl and gitignore

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 import re
+import openpyxl
 
-rawf = open("The_Great_Gatsby_short.txt", "r")
-raw = rawf.read()
-raw = raw.lower()
+rawf = open("The_Great_Gatsby.txt", "r")
+raw = rawf.read() #string data of whole text
+raw = raw.lower() #all capital letters to small letters
 
-# text_list = re.findall("\W", raw)
-# for i in text_list:
-#     print(i)
+text_list = re.split("\W", raw) #list of constituent words
+text_list = list(filter(lambda x: x != "", text_list)) #deletes empty strings
 
-text_list = re.split("\W", raw)
-text_list = list(filter(lambda x: x != "", text_list))
+wb = openpyxl.Workbook() #new workbook
+ws = wb.active
 
-for i in text_list:
-    print(i)
-
-result = open("result.txt", "w")
-
-freq={}
+freq={} #dictionary that contains words and corresponding frequencies
 for word in text_list:
-    if freq.get(word) == None:
+    if freq.get(word) == None: #if the word isn't checked yet, initialize it with a frequency of 1
         freq[word] = 1
-    freq[word] += 1
-    result.write(f"{word} {freq[word]}\n")
+    freq[word] += 1 #new detection, plus one for frequency
 
-result.close
+row = 1
+for word, freq in freq.items():
+    ws.cell(row = row, column = 1).value = word #left column with words
+    ws.cell(row = row, column = 2).value = freq #right column with corresponding frequencies
+    row += 1
 
-ref = open("re_text.txt", "w")
-for i in text_list:
-    ref.write(i+" ")
-ref.close()
+wb.save(filename = "result.xlsx") #save the result worksheet
+wb.close()
+rawf.close()
